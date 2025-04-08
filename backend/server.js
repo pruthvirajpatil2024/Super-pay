adbd6b019fce496502b867834e35f886077ad300
Pruthviraj Patil <pruthvirajpatil2024@gmail.com>
2025-04-08 23:33:06 +0530

    Add zod validation and get route for users

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,21 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 
 // ENV variables
+<<<<<<< HEAD
 const PORT =  process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 // All routes.
 const userRoutes = require("./routes/UserRoutes");
+=======
+const PORT =  process.env.PORT || 5001;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// All routes.
+
+const rootRouter = require("./routes/index");
+
+>>>>>>> b58a5bc (Add zod validation and get route for users)
 
 const app = express();
 app.use(express.json());
@@ -20,7 +30,13 @@ mongoose.connect(MONGODB_URI).then(()=>{
     console.log('Unable to connect database');
 })
 
+<<<<<<< HEAD
 app.use('/user', userRoutes);
+=======
+app.use('/api/v1', rootRouter);
+
+
+>>>>>>> b58a5bc (Add zod validation and get route for users)
 
 app.get('/',(req, res)=>{
     res.send('Hello World');
